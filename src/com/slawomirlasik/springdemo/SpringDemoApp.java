fbf10d44021aebe623776ff7cbd95ecc040c9699
Sławomir Lasik <--global>
2020-04-29 20:43:36 +0200

    Created example showcasing how to change scopes using XML configuration

--- src/com/slawomirlasik/springdemo/SpringDemoApp.java
+++ src/com/slawomirlasik/springdemo/SpringDemoApp.java
@@ -18,6 +18,22 @@ public class SpringDemoApp {
 		// get the email and the school
 		System.out.println("Email: " + slawkoTalker.getEmail());
 		System.out.println("School: " + slawkoTalker.getSchool());
+		
+		// get the other slawko speaker
+		SlawkoSpeaker otherSlawkoSpeaker = context.getBean("slawkoSpeaker", SlawkoSpeaker.class);
+		
+		// check if they are twins
+		boolean twins = (slawkoTalker == otherSlawkoSpeaker);
+		
+		// print the message about slawko twin status
+		System.out.println();
+		System.out.println( twins ? "They are twins" : "They are not twins");
+		
+		System.out.println();
+		System.out.println("Proof:");
+		System.out.println(slawkoTalker);
+		System.out.println(otherSlawkoSpeaker);
+		System.out.println();
 
 		// close context
 		context.close();
