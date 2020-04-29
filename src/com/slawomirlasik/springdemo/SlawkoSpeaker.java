1a28eee79114164c78cc1ac11d3344e8d5f01e72
Sławomir Lasik <--global>
2020-04-29 20:20:18 +0200

    Created example of XML Spring Config with method injection

--- src/com/slawomirlasik/springdemo/SlawkoSpeaker.java
+++ src/com/slawomirlasik/springdemo/SlawkoSpeaker.java
@@ -4,10 +4,17 @@ public class SlawkoSpeaker implements Talker {
 
 	private SpeachService speachService;
 
-	public SlawkoSpeaker(SpeachService speachService) {
+	public SlawkoSpeaker() {
 		super();
+	}
+
+	
+	
+	public void setSpeachService(SpeachService speachService) {
 		this.speachService = speachService;
 	}
+
+
 
 	@Override
 	public void giveSpeach() {
