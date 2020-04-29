81a7de6a2a84f9d7b88618a897fa5204cec7ba94
Sławomir Lasik <--global>
2020-04-29 21:01:04 +0200

    Created example using XML Config to show the usage of the init and destroy methods of the bean in xml file

--- src/com/slawomirlasik/springdemo/ShortSpeachService.java
+++ src/com/slawomirlasik/springdemo/ShortSpeachService.java
@@ -7,4 +7,18 @@ public class ShortSpeachService implements SpeachService {
 		return "Today I Had a dream. But I do not remember it";
 	}
 
+	public void checkTheTyposInSpeach() {
+		System.out.println("------------------------------------");
+		System.out.println("Checking the typos in speach...");
+		System.out.println("No typos found.");
+		System.out.println("------------------------------------");
+	}
+
+	public void burnThisSpeach() {
+		System.out.println("------------------------------------");
+		System.out.println("Burning this speach for good...");
+		System.out.println("------------------------------------");
+
+	}
+
 }
