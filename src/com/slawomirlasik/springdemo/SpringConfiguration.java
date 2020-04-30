181d81556674c48a2c8a25640f7efea5200ea0b0
Sławomir Lasik <--global>
2020-04-30 16:02:19 +0200

    Created example of Spring configuration using java configuration class

---
+++ src/com/slawomirlasik/springdemo/SpringConfiguration.java
@@ -0,0 +1,10 @@
+package com.slawomirlasik.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.slawomirlasik.springdemo") // the path to scan is optional)
+public class SpringConfiguration {
+
+}
--- src/com/slawomirlasik/springdemo/SpringDemoApp.java
+++ src/com/slawomirlasik/springdemo/SpringDemoApp.java
@@ -1,14 +1,14 @@
 package com.slawomirlasik.springdemo;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringDemoApp {
 
 	public static void main(String[] args) {
 
 		// get context from spring
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(SpringConfiguration.class);
 		// get talker
 		Talker slawkoTalker = context.getBean("slawkoSpeaker", Talker.class);
 
