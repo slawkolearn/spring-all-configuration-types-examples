b144c53ae057a306bd8e90fd8a21f3beaade7407
Sławomir Lasik <--global>
2020-04-30 14:27:00 +0200

    Created example of using java annotation for bean scope configuration

--- src/com/slawomirlasik/springdemo/SlawkoSpeaker.java
+++ src/com/slawomirlasik/springdemo/SlawkoSpeaker.java
@@ -1,11 +1,14 @@
 package com.slawomirlasik.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("singleton")
+@Scope("prototype")
 public class SlawkoSpeaker implements Talker {
 
 	private SpeachService speachService;
@@ -19,6 +22,18 @@ public class SlawkoSpeaker implements Talker {
 	@Override
 	public void giveSpeach() {
 		System.out.println(speachService.getSpeach());
+	}
+	
+	// from java 9+ the @PostContruct @PreDestroy annotations are gone and have to be added to classpath manually
+	@PostConstruct
+	public void doPreparationForSpeach() {
+		System.out.println("Shawing legs. Doing A O E I U ... ");
+	}
+	
+	// !!! for the prototype scope beans spring do not call the PreDestroy methods.!!!
+	@PreDestroy
+	public void doMyCleanUpStuff() {
+		System.out.println("Cleaning the speach place..");
 	}
 
 }
