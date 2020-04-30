2ffcda524a893593f08166235ce54eef6fea1ad6
Sławomir Lasik <--global>
2020-04-30 13:54:30 +0200

    created example showcasing how to use value injection from file using java annotations

--- src/com/slawomirlasik/springdemo/SomeFunnyOneLineSpeach.java
+++ src/com/slawomirlasik/springdemo/SomeFunnyOneLineSpeach.java
@@ -1,13 +1,17 @@
 package com.slawomirlasik.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SomeFunnyOneLineSpeach implements SpeachService {
+	
+	@Value("${foo.joke}")
+	private String jokeFromFile;
 
 	@Override
 	public String getSpeach() {
-		return "\n-----------------\nQ: What did the SNAIL say while riding on the turtles back?\nA: Wheeeeeeeee\n-------------";
+		return jokeFromFile;
 	}
 
 }
