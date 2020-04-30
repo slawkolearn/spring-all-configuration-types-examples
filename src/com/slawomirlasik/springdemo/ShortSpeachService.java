09382479bae559d30be3117905b17ee967dc8ffe
Sławomir Lasik <--global>
2020-04-30 16:12:38 +0200

    Created example how to inject values from property file to fields using java configuration class

--- src/com/slawomirlasik/springdemo/ShortSpeachService.java
+++ src/com/slawomirlasik/springdemo/ShortSpeachService.java
@@ -1,13 +1,17 @@
 package com.slawomirlasik.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("myServiceComponentManuallySet")
 public class ShortSpeachService implements SpeachService {
+	
+	@Value("${foo.speach}")
+	private String speach;
 
 	@Override
 	public String getSpeach() {
-		return "Today I Had a dream. But I do not remember it";
+		return speach;
 	}
 
 }
--- src/com/slawomirlasik/springdemo/SpringConfiguration.java
+++ src/com/slawomirlasik/springdemo/SpringConfiguration.java
@@ -2,8 +2,10 @@ package com.slawomirlasik.springdemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:speach.properties")
 // notice that we do not do the component scan anymore
 public class SpringConfiguration {
 	
